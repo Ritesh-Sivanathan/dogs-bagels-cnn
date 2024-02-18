866134090f99c9da430598eacbeb28eb4a1ed4d2
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2024-02-18 16:35:46 -0500

    Update classifier.py

--- dogs vs bagels/model/classifier.py
+++ dogs vs bagels/model/classifier.py
@@ -5,23 +5,20 @@ import os
 
 
 for _ in range(1):
-    # Load the saved model
+
     model_path = f'{os.getcwd()}\\model\\model_weights.h5'
     model = load_model(model_path)
 
-    # Load the image
     img_path = f'{os.getcwd()}\\model\\very-cute-dog.jpg'
-    img = image.load_img(img_path, target_size=(406, 612))  # Resize to match model input shape
+    img = image.load_img(img_path, target_size=(406, 612))
 
-    # Convert image to array and preprocess
     img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    img_array = img_array / 255.0  # Normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0) 
+    img_array = img_array / 255.0 
 
-    # Perform inference using the loaded model
+    
     prediction = model.predict(img_array)
     print(prediction)
-    # Interpret the prediction
     if prediction[0][0] >= 0.5:
         print("The model classified the image as a dog.")
     else:
